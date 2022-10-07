f02e888a534af2cf5024cbf959748bc5400ca3cd
Philipp <github@philipp.philippleser.de>
2022-10-07 11:47:06 +0200

    add home-manager channel version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-      home-manager.url = "github:nix-community/home-manager";
+      home-manager.url = "github:nix-community/home-manager/release-22.05";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
       # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
       darwin.url = "github:lnl7/nix-darwin";
