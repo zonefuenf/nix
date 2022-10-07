2bd4fff96cabcb54438bb5f282348839191999f2
Philipp <github@philipp.philippleser.de>
2022-10-07 13:28:37 +0200

    packages

--- flake.nix
+++ flake.nix
@@ -2,24 +2,24 @@
   description = "macOS setup";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
-      home-manager.url = "github:nix-community/home-manager/release-22.05";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
-      darwin.url = "github:lnl7/nix-darwin";
-      darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+# nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs"; # ...
   };
-  
-  # add the inputs declared above to the argument attribute set
-  outputs = { self, nixpkgs, home-manager, darwin }: {
-  darwinConfigurations."majorana" = darwin.lib.darwinSystem {
-  # you can have multiple darwinConfigurations per flake, one per hostname
 
-    system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac
-    modules = [
-home-manager.darwinModules.home-manager
-    ./hosts/majorana/default.nix 
-    ]; # will be important later
-  };
+# add the inputs declared above to the argument attribute set
+  outputs = { self, nixpkgs, home-manager, darwin }: {
+    darwinConfigurations."majorana" = darwin.lib.darwinSystem {
+# you can have multiple darwinConfigurations per flake, one per hostname
+
+      system = "aarch64-darwin"; # "x86_64-darwin" if you're using a pre M1 mac
+        modules = [
+          home-manager.darwinModules.home-manager
+          ./hosts/majorana/default.nix 
+        ]; # will be important later
+    };
   };
 }
--- hosts/majorana/default.nix
+++ hosts/majorana/default.nix
@@ -1,20 +1,65 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-
-  # Make sure the nix daemon always runs
   services.nix-daemon.enable = true;
-programs.zsh.enable = true;
-home-manager.useGlobalPkgs = true;
-home-manager.useUserPackages = true;
-home-manager.users.pleser = { pkgs, ... }: {
-  home.stateVersion = "22.05";
-};
-homebrew = {
-  enable = true;
-  autoUpdate = true;
-  casks = [
-    "iina"
-  ];
-};
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    initExtra = ''
+      eval "$(starship init zsh)";
+    '';
+  };
+  home-manager.users.pleser = { pkgs, ... }: {
+    home.stateVersion = "22.05";
+    home.packages = with pkgs; [
+      starship
+        neovim
+        mc
+        wget
+        tmux
+        tree-sitter
+    ];
+  };
+  homebrew = {
+    onActivation.enable = true;
+    onActivation.autoUpdate = true;
+    casks = [
+      "adobe-creative-cloud"
+        "discord-canary"
+        "iina"
+        "mountain"
+        "steam"
+        "affinity-designer"
+        "docker"
+        "iterm2"	
+        "omnigraffle"		
+        "telegram"
+        "affinity-photo"
+        "epic-games"	
+        "jetbrains-toolbox"
+        "onedrive"		
+        "timemachineeditor"
+        "alfred"			
+        "firefox"			
+        "qmk-toolbox"		
+        "visual-studio-code"
+        "backblaze"		
+        "fujitsu-scansnap-home"
+        "latexit"	
+        "whatsapp"
+        "balenaetcher"	
+        "github"		
+        "microsoft-auto-update"
+        "soulver"	
+        "zoom"
+        "carbon-copy-cloner"
+        "hazel"	
+        "microsoft-edge"	
+        "soundsource"
+        ];
+  };
 
 }
